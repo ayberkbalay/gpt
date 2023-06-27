30b06df87319b56f195806f08d4f06b8431a4ce6
ayberkbalay <91059409+ayberkbalay@users.noreply.github.com>
2023-06-27 13:13:20 +0300

    loading and checking fetching

--- src/components/demo.jsx
+++ src/components/demo.jsx
@@ -16,7 +16,16 @@ const demo = () => {
   const [getSummary, { error, isFetching }] =
     useLazyGetSummaryQuery();
 
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem('articles')
+    );
 
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+
+  }, []);
   const handleSubmit = async (e) => {
     const { data } = await getSummary({
       articleUrl: article.url
@@ -27,49 +36,91 @@ const demo = () => {
 
       setArticle(newArticle)
       setAllArticles(updatedArticles);
-      console.log(newArticle)
+      localStorage.setItem('articles', JSON.stringify(updatedArticles));
 
     }
   }
 
 
-    return (
-      <section className="mt-16 w-full max-w-xl ">
-        {/* Search bar */}
-        <div className="flex flex-col w-full gap-2">
-          <h2 className="text-2xl font-bold">Summarize any article</h2>
-          <form
-            className="relative flex justify-center items-center"
-            onSubmit={handleSubmit}
-          >
-            <img
-              src={linkIcon}
-              alt="link icon"
-              className="absolute left-0 my-2 ml-3 w-5">
-            </img>
-            <input
-              type='search'
-              id="search"
+  return (
+    <section className="mt-16 w-full max-w-xl ">
+      {/* Search bar */}
+      <div className="flex flex-col w-full gap-2">
+        <h2 className="text-2xl font-bold">Summarize any article</h2>
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link icon"
+            className="absolute left-0 my-2 ml-3 w-5">
+          </img>
+          <input
+            type='search'
+            id="search"
 
-              placeholder='Paste the article link'
-              value=""
-              onChange={(e) => setArticle({ ...article, url: e.target.value })}
-              required
-              className='url_input peer' // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
-            />
-            <button
-              type="submit"
-              className="submit_btn
+            placeholder='Paste the article link'
+            value=""
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className='url_input peer' // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
+          />
+          <button
+            type="submit"
+            className="submit_btn
              peer-focus:border-gray-700
              peer-focus:text-gray-700">
-              <p>↩</p>
-            </button>
-          </form>
-          {/* Browse URL History */}
+            <p>↩</p>
+          </button>
+        </form>
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((article, index) => (
+            <div
+              key={'link${index}'}
+              onclick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn">
+                <img src={copy} alt="copy_icon"
+                  className="w-[%40] h-[%40] object-contain" />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium">
+                {item.url}
+              </p>
+            </div>
+
+          ))}
         </div>
-        {/* Result */}
-      </section>
-    )
-  
+        {/* Browse URL History */}
+      </div>
+      <div className="my-10 max-w-full flex justify-center items-center"> 
+            {isFetching ? (
+              <img src={loader} alt="loader" className="w-10 h-10" />
+            ) : error ? (
+              <p className="font-inter font-bold text-black text-center">
+                That wasn't suppose to happen
+                
+                <br />
+                <span>  
+                  {error?.data?.error}
+                </span>
+                </p>
+            ): (
+              article.summary && (
+                <div className="flex flex-col gap-2">
+                <h2>Article <span> Summary</span></h2>
+                <div className="summary_box">
+                  <p>{article.summary}</p>
+
+              </div>
+          </div>
+              )  
+            )}
+      </div>
+      {/* Result */}
+    </section>
+  )
+
 }
 export default demo
